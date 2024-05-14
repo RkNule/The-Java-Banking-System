38a8dd716c4cd210ad79749a3a38a712419d9f0d
Rohan Nule <151975572+RkNule@users.noreply.github.com>
2024-05-14 17:05:54 +0530

    Create CreditDebitRequest.java

---
+++ CreditDebitRequest.java
@@ -0,0 +1,16 @@
+package com.banking.jbs.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditDebitRequest {
+	private String accountNumber;
+	private BigDecimal amount;
+
+}
