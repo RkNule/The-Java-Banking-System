153135dc123f806116856f9e799e6ca9944869d8
Rohan Nule <151975572+RkNule@users.noreply.github.com>
2024-05-14 17:35:35 +0530

    Create TheJavaBankingSystemApplicationTests.java

---
+++ TheJavaBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.jbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
