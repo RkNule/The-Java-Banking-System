5d944b1662f24dfc6b81722d2567ccf768766afa
Rohan Nule <151975572+RkNule@users.noreply.github.com>
2024-05-14 17:26:06 +0530

    Create CustomUserDetailsService.java

---
+++ CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.banking.jbs.service.impl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.banking.jbs.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private UserRepository userRepository;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username + "not found"));
+	}
+
+}
