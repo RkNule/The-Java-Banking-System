a86c324b585f0f396989d749efdd9f6852ed8a31
Rohan Nule <151975572+RkNule@users.noreply.github.com>
2024-05-14 17:03:59 +0530

    Create AccountInfo.java

---
+++ AccountInfo.java
@@ -0,0 +1,33 @@
+package com.banking.jbs.dto;
+
+import java.math.BigDecimal;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class AccountInfo {
+	
+	@Schema(
+			name = "User Account Name"
+			)
+	private String accountName;
+	
+	@Schema(
+			name = "User Account Balance"
+			)
+	private BigDecimal accountBalance;
+	
+	@Schema(
+			name = "User Account Number"
+			)
+	private String accountNumber;
+
+}
